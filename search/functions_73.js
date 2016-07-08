611060207fe31d572ac76945543792ab0616d94c
Xun Wang <wang.xun@gmail.com>
2016-07-08 12:21:53 +1000

    doc refresh.

--- search/functions_73.js
+++ search/functions_73.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['sendmessagetonode',['sendMessageToNode',['../classPyREEM.html#a437882718bd29bdbd0ce0b827c990051',1,'PyREEM']]],
   ['setarmstiffness',['setArmStiffness',['../classPyREEM.html#a637a770d22b3c66e21df137720d0b879',1,'PyREEM']]],
   ['setearled',['setEarLED',['../classPyREEM.html#a01426e4ea139de985bdc2d69fdc6f046',1,'PyREEM']]],
   ['sethandposition',['setHandPosition',['../classPyREEM.html#af56dc8d51afafbfe02cc906cc8da297e',1,'PyREEM']]],
@@ -8,5 +9,5 @@ var searchData=
   ['startdatarecording',['startDataRecording',['../classPyREEM.html#ab936f344fa76977901ac56a73891e78b',1,'PyREEM']]],
   ['startpalfaceenrollment',['startPalFaceEnrollment',['../classPyREEM.html#a26b2cbf32895f1cc18882b400b611436',1,'PyREEM']]],
   ['stopdatarecording',['stopDataRecording',['../classPyREEM.html#aabc8468057e6ccc57ea728674af370d6',1,'PyREEM']]],
-  ['stoppalfaceenrollment',['stopPalFaceEnrollment',['../classPyREEM.html#ab27de34b81c38c91d65b350ccbcb7dce',1,'PyREEM']]]
+  ['stoppalfaceenrollment',['stopPalFaceEnrollment',['../classPyREEM.html#a090b472230dbd423fd595723a3b68446',1,'PyREEM']]]
 ];
